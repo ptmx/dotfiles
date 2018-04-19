f67d3c0ab22b3820ca823e1033dfafe121201d39
Josh Zucker <zuckerje@gmail.com>
2018-04-19 14:12:35 -0400

    git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -22,8 +22,9 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
-alias gpf='git push --force-with-lease'
-alias gcob='git checkout -b'
+
+# Mine
+alias gprune='git fetch --prune origin'
 
 # git add everything and commit it with 'wip' msg
 function gwip {
