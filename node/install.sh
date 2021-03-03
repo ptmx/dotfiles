898554529964ded265ee46124d45e02edcab02de
Josh Zucker <zuckerje@gmail.com>
2021-03-03 09:24:00 -0500

    Add nvm

--- node/install.sh
+++ node/install.sh
@@ -1,5 +1,9 @@
 # simple file server
+
 if test ! $(which serve)
 then
   sudo npm install serve -g
 fi
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
